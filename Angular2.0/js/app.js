fdf7af3d72b089c3be9b96e8c9ccbe062871c3a0
Max <maxim1006@list.ru>
2016-02-19 21:50:23 +0300

    removed redundant files

--- Angular2.0/js/app.js
+++
@@ -1,5 +0,0 @@
-// Declare app level module which depends on ngRoute
-angular.module('NoteWrangler', ['ngRoute', 'ngResource', 'Gravatar'])
-.config(function($gravatarProvider){
-  $gravatarProvider.setSize(100);
-});
--- Angular2.0/js/services/markdown.js
+++
@@ -1,9 +0,0 @@
-// In level 3 they need to refactor the code from nw-card.js into a Service:
-
-angular.module('NoteWrangler').factory( 'markdown',  function markdownFactory(){
-  return {
-    parse: function(text){
-      return markdown.toHTML(text);
-    }
-  }
-});
